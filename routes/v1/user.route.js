d7e5fff599690d9f0302b508ef328e4de12f1838
girish <gsaundarkar@gmail.com>
2025-04-27 18:03:51 +0530

    user feature implemented

---
+++ routes/v1/user.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userController = require('../../controllers/user.controller')
+const router = express.Router();
+
+router.route('/').get(userController.getAll).post(userController.createUser);
+
+router.route('/:id')
+    .get(userController.getUserById)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
+
+module.exports = router
