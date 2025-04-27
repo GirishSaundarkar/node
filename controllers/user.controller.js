9417b67ce961a296dcda4e5ee09a06b919333f8e
girish <gsaundarkar@gmail.com>
2025-04-27 18:23:47 +0530

    implemented middleware to catch aynchronouse errors

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,30 +1,31 @@
-const User = require('../models/user.model')
+const User = require('../models/user.model');
+const CatchAsync = require('../middlewares/CatchAsync')
 
-const getAll = async () => {
+const getAll = CatchAsync(async () => {
     const user = await User.Create(req.body);
     res.send(user);
-};
+});
 
-const getUserbyId = async () => {
+const getUserbyId = CatchAsync(async () => {
     const user = await User.findById(req.params.id)
     res.send(user);
-};
+});
 
-const createUser = async () => {
+const createUser = CatchAsync(async () => {
     const user = await User.create(req.body)
     res.send(user);
 
-}
+})
 
-const updateUser = async () => {
+const updateUser = CatchAsync(async () => {
     const user = await User.findOneAndUpdate(req.user.id);
     res.send(user)
-}
+})
 
-const deleteUser = async () => {
+const deleteUser = CatchAsync(async () => {
     const user = await User.delete(req.user.id);
     res.send(user)
-}
+})
 
 module.exports = {
     getAll,
