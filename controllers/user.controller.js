71f02f9a31875f9655a9b5d00e0d2b4bb0f5d0ce
girish <gsaundarkar@gmail.com>
2025-04-30 10:59:34 +0530

    bug fixes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,12 +2,12 @@ const User = require('../models/user.model');
 const CatchAsync = require('../middlewares/CatchAsync')
 
 const getAll = CatchAsync(async () => {
-    const user = await User.Create(req.body);
+    const user = await User.find();
     res.send(user);
 });
 
-const getUserbyId = CatchAsync(async () => {
-    const user = await User.findById(req.params.id)
+const getUserById = CatchAsync(async () => {
+    const user = await User.findById(req.user.id)
     res.send(user);
 });
 
@@ -23,13 +23,13 @@ const updateUser = CatchAsync(async () => {
 })
 
 const deleteUser = CatchAsync(async () => {
-    const user = await User.delete(req.user.id);
+    const user = await User.findOneAndUpdate(req.user.id);
     res.send(user)
 })
 
 module.exports = {
     getAll,
-    getUserbyId,
+    getUserById,
     createUser,
     updateUser,
     deleteUser
