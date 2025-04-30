c94b0a5f940e18deabb4948e2ff76d454ffe39d1
girish <gsaundarkar@gmail.com>
2025-04-30 11:56:46 +0530

    error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,43 @@
 const Tour = require('../models/tour.model');
-const CatchAsync = require('../middlewares/CatchAsync')
+const CatchAsync = require('../middlewares/CatchAsync');
+const AppError = require('../utils/appError');
 
 const getAll = CatchAsync(async () => {
-    const tour = await Tour.find();
+    const tour = await Tour.find()
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
     res.send(tour);
 });
 
-const getTourById = CatchAsync(async () => {
+const getTourById = CatchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id)
+    if (!tour) {
+        return next(new AppError('NO tour found with that ID', 404));
+    }
     res.send(tour);
 });
 
-const createTour = CatchAsync(async () => {
+const createTour = CatchAsync(async (req, res) => {
     const tour = await Tour.create(req.body)
     res.send(tour);
 
 })
 
-const updateTour = CatchAsync(async () => {
+const updateTour = CatchAsync(async (req, res) => {
     const tour = await Tour.findOneAndUpdate(req.params.id);
+    if (!tour) {
+        return next(new AppError('NO tour found with that ID', 404));
+    }
     res.send(tour)
 })
 
-const deleteTour = CatchAsync(async () => {
+const deleteTour = CatchAsync(async (req, res) => {
     const tour = await Tour.findOneAndUpdate(req.params.id);
+    if (!tour) {
+        return next(new AppError('NO tour found with that ID', 404));
+    }
     res.send(tour)
 })
 
